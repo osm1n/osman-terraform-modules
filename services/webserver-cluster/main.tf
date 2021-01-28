5c9f228c62890fee1db096a27612929736aee2d9
ussy <osman.garba@gmail.com>
2021-01-28 08:15:30 +0800

    Just some few changes

--- services/webserver-cluster/main.tf
+++ services/webserver-cluster/main.tf
@@ -210,7 +210,7 @@ resource "aws_security_group_rule" "allow_http_inbound" {
 # create egress security rule 
 resource "aws_security_group_rule" "allow_all_outbound" {
   type = "egress"
-  security_group_id = aws_security_group_alb.id
+  security_group_id = aws_security_group.alb.id
 
   from_port   = local.any_port
   to_port     = local.any_port
